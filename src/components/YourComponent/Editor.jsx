608b1657cb228bb51371916515d156612223b843
MeXaR <f.aubeut@gmail.com>
2017-06-24 14:05:21 +0200

    update

--- src/components/YourComponent/Editor.jsx
+++ src/components/YourComponent/Editor.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import autoBind                     from "react-autobind"
 import TinyMCE                      from 'react-tinymce';
 import {flatener}                   from "./lib/flatener" 
+import {
+  Button, 
+  Dimmer, 
+  Loader
+}                           from 'semantic-ui-react'
 
 export default class Editor extends Component {
 
@@ -9,27 +14,41 @@ export default class Editor extends Component {
         super(props)
         autoBind(this)
         this.state = {
-        content: null
+            content: null
         }
     }
 
     handleEditorChange(e) {
-        
         const content = e.target.getContent()
-        this.setState({content})
         const flatenedContent = flatener({content})
         this.props.getContent(flatenedContent)
         console.log(e, flatenedContent)
+        this.setState({content: flatenedContent.content})
+    }
+
+    formatSrc(){
+        this.setState({ loading: true })
+        setTimeout(() => {
+            this.setState({ loading: false })
+        }, 500);
     }
 
 
     render() {
 
-        const { content } = this.state
+        const { content, loading } = this.state
+
+        if(loading){
+            return(
+                <Dimmer active style={{ position: "absolute" }}>
+                    <Loader/>
+                </Dimmer>
+            )
+        }
 
         return (
         <div>
-                <TinyMCE
+            <TinyMCE
                 content={this.state.content || ""}
                 config={{
                     height: 400,
@@ -62,51 +81,11 @@ export default class Editor extends Component {
                     ]
                 }}
                 onChange={this.handleEditorChange}
-                />
+            />
+
+            <Button onClick={ this.formatSrc } content="Rendre accessible" color="teal" style={{marginTop:15}}/>
 
         </div>
         );
     }
 }
-
-  const textcolors = [
-    "000000", "Black",
-    "993300", "Burnt orange",
-    "333300", "Dark olive",
-    "003300", "Dark green",
-    "003366", "Dark azure",
-    "000080", "Navy Blue",
-    "333399", "Indigo",
-    "333333", "Very dark gray",
-    "800000", "Maroon",
-    "FF6600", "Orange",
-    "808000", "Olive",
-    "008000", "Green",
-    "008080", "Teal",
-    "0000FF", "Blue",
-    "666699", "Grayish blue",
-    "808080", "Gray",
-    "FF0000", "Red",
-    "FF9900", "Amber",
-    "99CC00", "Yellow green",
-    "339966", "Sea green",
-    "33CCCC", "Turquoise",
-    "3366FF", "Royal blue",
-    "800080", "Purple",
-    "999999", "Medium gray",
-    "FF00FF", "Magenta",
-    "FFCC00", "Gold",
-    "FFFF00", "Yellow",
-    "00FF00", "Lime",
-    "00FFFF", "Aqua",
-    "00CCFF", "Sky blue",
-    "993366", "Red violet",
-    "FFFFFF", "White",
-    "FF99CC", "Pink",
-    "FFCC99", "Peach",
-    "FFFF99", "Light yellow",
-    "CCFFCC", "Pale green",
-    "CCFFFF", "Pale cyan",
-    "99CCFF", "Light sky blue",
-    "CC99FF", "Plum"
-  ]
--- src/components/YourComponent/lib/flatener.js
+++ src/components/YourComponent/lib/flatener.js
@@ -1,29 +1,35 @@
 import htmlparser from "htmlparser2"
 
+const toKeep = [ "ul", "ol", "li" ]
+const titles = [ "h1", "h2", "h3", "h4", "h5", "h6" ]
+
 export const flatener = ({content}) => {
     let result= ""
     const parser = new htmlparser.Parser({
         onopentag: (tagname, attribs) => {
-            if(tagname === "div"){
-                result += "<p>"
-            } else {
+            if(titles.indexOf(tagname) !== -1){
+                result += "</p><"+tagname+">"
+            } else if (toKeep.indexOf(tagname) !== -1){
                 result += "<"+tagname+">"
+            } else if( tagname === "img"){
+                console.log(attribs)
+                result += "<p><img src=" + attribs.src + " alt=" + attribs.alt + " class=" + attribs.class + " height=" + attribs.height + " width=" +attribs.width + " /></p>"
             }
         },
         ontext: (text) => {
             result += text
-            console.log("-->", text);
         },
         onclosetag: (tagname) => {
-            if(tagname === "div"){
-                result += "</p>"
-            } else {
+            if(titles.indexOf(tagname) !== -1){
+                result += "</"+tagname+"><p>"
+            } else if (toKeep.indexOf(tagname) !== -1){
                 result += "</"+tagname+">"
             }
         }
     }, {decodeEntities: true});
         parser.write(content);
         parser.end();
+        result = result.replace(/\n|\r|\t|\f\[\b]/gm, "").replace("<p></p>", "") + "</p>"
         
         return { content: result }
 }
